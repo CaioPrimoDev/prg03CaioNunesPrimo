18feeba5a07bdcb9c26f888f38836f2859d2dda8
CaioPrimoDev <caioprimoedi@gmail.com>
2025-04-30 23:03:23 -0300

    ADD |ativiadade 03| - Rescrevendo o projeto em Ant

---
+++ CalculadoraIdade/src/br/com/ifba/principal/Menu.java
@@ -0,0 +1,30 @@
+package br.com.ifba.principal;
+
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        
+        @SuppressWarnings("UnusedAssignment")
+        int escolha = 1;
+        Scanner scanner = new Scanner(System.in);        
+        
+        do {
+            
+            System.out.println("\n\nSelecione a atividade (0 para fechar): ");
+            System.out.println("1- ..... [ATIVIDADE JA ENVIADA]");
+            System.out.println("2- ..... [ATIVIDADE JA ENVIADA]");
+            System.out.print("3- Calculadora de Idade\n> ");
+            escolha = scanner.nextInt();
+        
+            switch(escolha) {
+                case 0 -> System.out.println("\n\n === Menu finalizado === \n\n");
+                case 1, 2 -> System.out.println("\n\n Atividade já enviada \n\n");
+                case 3 -> // Chamando a main da interfaceIdade
+                    br.com.ifba.atividade03.view.interfaceIdade.main(args);
+                default -> System.out.println("\n\n Erro: Selecione uma opcao valida \n\n");
+            }
+            
+        } while(escolha != 0);
+    }
+}
